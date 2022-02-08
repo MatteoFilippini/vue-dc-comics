3f9659b23ae5859f40646738728c53df5331eab0
MatteoFilippini <matteofilippini8@gmail.com>
2022-02-08 17:14:07 +0100

    added menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1>sdasayfds</h1>
+  <section id="icons">
+    <div class="card">
+      <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+      <h4>DIGITAL COMICS</h4>
+    </div>
+    <div class="card">
+      <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+      <h4>DC MERCHANDISE</h4>
+    </div>
+    <div class="card">
+      <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+      <h4>SUBSCRIPTION</h4>
+    </div>
+    <div class="card">
+      <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+      <h4>COMICS SHOP LOCATOR</h4>
+    </div>
+    <div class="card">
+      <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+      <h4>DC POWER VISA</h4>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -8,5 +29,24 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+#icons {
+  background-color: blue;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  color: white;
+  padding: 30px 0;
+  .card {
+    display: flex;
+    align-items: center;
+    font-size: 0.8rem;
+    h4 {
+      margin-left: 10px;
+    }
+    img {
+      height: 40px;
+    }
+  }
+}
 </style>
