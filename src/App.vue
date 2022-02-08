ac93aa4448fd64c86621ce928e9ef1f7e6522ed6
MatteoFilippini <matteofilippini8@gmail.com>
2022-02-08 16:24:08 +0100

    created header layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <div>
-    <header></header>
+  <div class="container">
+    <Header />
     <main></main>
     <footer></footer>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  margin: 0 auto;
+  width: 1080px;
+  max-width: 100%;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <nav>
+      <figure>logo</figure>
+      <ul>
+        <li>s</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+li {
+  list-style-type: none;
+}
+</style>
